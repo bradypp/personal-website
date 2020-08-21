5a5b4906ac0aadfce7c9edb19fea27de0299ee3e
bradypp <bradypp44@gmail.com>
2020-08-22 00:41:53 +0100

    Added some draft blog posts

--- src/components/blog/posts.js
+++ src/components/blog/posts.js
@@ -12,6 +12,7 @@ const PostsContainer = styled.section`
     display: grid;
     grid-template-columns: repeat(2, 50%);
     grid-gap: 2rem;
+    width: 100%;
 
     ${media.bp800`
         grid-template-columns: 100%;
